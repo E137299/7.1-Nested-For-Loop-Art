91654ae5121b10ce347bece49a36807fc491e33f
David Yin <29613693+E137299@users.noreply.github.com>
2024-01-11 17:26:01 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from colorama import Fore, Back
+# Color Options: RED, LIGHTRED_EX, YELLOW, LIGHTYELLOW_EX, GREEN, LIGHTGREEN_EX, CYAN, LIGHTCYAN_EX, BLUE, LIGHTBLUE_EX,MAGENTA, LIGHTMAGENTA_EX, WHITE, LIGHTWHITE_EX, BLACK, LIGHTBLACK_EX
+
+def draw_7():
+	pass
+print("Draw 7")
+draw_7()
+
+
+def stars_and_stripes():
+	pass
+print("\nStars and Stripes")
+stars_and_stripes()
+
+
+def columns():
+	pass
+print("\nColumns")
+columns()
+
+
+def border():
+	pass
+print("\nBorder")
+border()
+
+
+def increasing_triangle():
+	pass
+print("\nIncreasing Triangles")
+increasing_triangle()
+
+
+def balanced_triangle():
+	pass
+print("\nBalanced Triangle")
+balanced_triangle()
+
+
+def diagonals():
+	pass
+print("\nDiagonals")
+diagonals()
